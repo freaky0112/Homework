f08f0eb20dcf9fad89d305d948817aefb841819f
freaky0112 <freakoutelf@gmail.com>
2016-02-23 22:02:53 +0800

    Advance Heap Sort Solved
    参考http://goo.gl/9NgbZT实现
    维护一个N大小的数组
    最后POP数组获得SUM
    顺便= =为何提交如此悲剧时好时坏，经常超时

--- data structure/Advanced Heap Sort/Advanced Heap Sort/main.cpp
+++ data structure/Advanced Heap Sort/Advanced Heap Sort/main.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 int length=0;
-void print_list(int length, long long *list){
+void print_list(int length, int *list){
     
     std::cout<<"\n";
     int n=1;
@@ -26,94 +26,155 @@ void print_list(int length, long long *list){
     
 }
 
-long top(long long* list){
+//合并数组
+int* merge(int* left, int* right, int left_length, int right_length){
+    int* list=new int[left_length+right_length];
+    int i=0,K=0,J=0;
+    while (left_length>0&&right_length>0) {
+        if (left[K]>right[J]) {
+            list[i]=right[J];
+            //right=delete_first_data(right,right_length);
+            right_length--;
+            i++;J++;
+        }else{
+            list[i]=left[K];
+            //left=delete_first_data(left,left_length);
+            left_length--;
+            i++;K++;
+        }
+    }
+    
+    while (left_length>0) {
+        list[i]=left[K];
+        //left=delete_first_data(left,left_length);
+        left_length--;
+        i++;K++;
+        //count++;
+    }
+    
+    while (right_length>0) {
+        list[i]=right[J];
+        //right=delete_first_data(right,right_length);
+        right_length--;
+        i++;J++;
+    }
+    return list;
+}
+
+int* merge_sort(int length, int* list){
+    //int count;
+    int right_length=length/2;
+    int left_length=length-right_length;
+    if (right_length<1) {
+        return list;
+    }
+    
+    int* right=new int[right_length];
+    int* left=new int[left_length];
+    int i=0;
+    for (i=0; i<length; i++) {
+        if (i<left_length) {
+            left[i]=list[i];
+        }else{
+            right[i-left_length]=list[i];
+        }
+    }
+    
+    right=merge_sort(right_length, right);
+    left=merge_sort(left_length, left);
+    list=merge(left, right, left_length, right_length);
+    
+    return list;
+}
+
+long  top(int* list){
     return list[0];
 }
 
 
-long long* get_child_node(long long* list,int current_postion){
-    long temp=list[current_postion];
-    int child_postion;
-    int right,left;
-    if (length>2*(current_postion+1)) {
-        left=2*(current_postion+1)-1;
-        right=2*(current_postion+1);
-        if (list[right]>list[left]) {
-            child_postion=left;
-        }else{
-            child_postion=right;
-        }
-    }else if (length==2*(current_postion+1)){
-        child_postion=length-1;
-    }else{
-        return  list;
-    }
-    if (temp> list[child_postion]) {
-        list[current_postion]=list[child_postion];
-        list[child_postion]=temp;
-        list=get_child_node(list, child_postion);
-    }
-    return list;
-}
-
-long long* get_father_node(long long* list,int current_postion){
-    long temp=list[current_postion];
-    int father_postion=(current_postion+1)/2-1;
-    if (current_postion==0||temp>list[father_postion]) {///顶层或者父节点小于自己
-        return list;
-    }
-    //当前节点与父节点交换位置
-    list[current_postion]=list[father_postion];
-    list[father_postion]=temp;
-    list=get_father_node(list, father_postion);
-    return list;
-}
-
-long long* pop(long long* list){
+int* pop(int* list){
     length--;
-    //long long* result=new long long[length];
-    list[0]=list[length];
-    //    for (int i=1; i<length; i++) {
-    //        result[i]=list[i];
-    //    }
-    //
-    list=get_child_node(list, 0);
+    int postion=0;//POP时初始节点为0节点
+    //int* result=new int[length];
+    list[postion]=list[length];//将0节点赋值为末位数
+    //list=get_child_node(list, 0);
+    int number;
+    while (true) {
+        int child_postion,right,left;
+        right=2*postion+2;
+        if (length>right) {//当有两个子节点时
+            left=right-1;
+            if (list[left]>list[right]) {//两个子节点取大值
+                child_postion=left;
+            }else{
+                child_postion=right;
+            }
+        }else if (length==right){//只有一个子节点时取左边的
+            child_postion=right-1;
+        }else{//没有子节点时跳出循环
+            break;
+        }
+        number=list[postion];
+        if (number>=list[child_postion]) {//当当前节点值大于等于子节点大的一个数的值时，跳出循环
+            break;
+        }else{//否则交换父节点与子节点的值
+            list[postion]=list[child_postion];
+            list[child_postion]=number;
+            postion=child_postion;
+        }
+        
+    }
     return  list;
 }
 
-long long* push(long long* list,long number){
+int* push(int* list,int number){
     int postion=length;
     length++;
-    //    long long* result=new long long[length];
+    //    int* result=new int[length];
     //    for (int i=0; i<length; i++) {
     //        result[i]=list[i];
     //    }
     list[postion]=number;
-    list=get_father_node(list, postion);
+    //list=get_father_node(list, postion);
+    while (postion>0) {
+        int father_postion=(postion+1)/2-1;
+        if (number<list[father_postion]) {//与父节点比较
+            break;                          //如果比父节点小,则跳出循环
+        }else{
+            list[postion]=list[father_postion];//否则将父结点与当前结点交换位置
+            list[father_postion]=number;
+            postion=father_postion;             //当前节点设为原节点父节点
+            number=list[postion];               //number为当前节点值
+        }
+    }
     return list;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int n,i;
+    int n,i,j;
+    long sum=0;
     cin>>n;
-    long long *list=new long long[n*n];
-    long* s=new long[n];
+    int *s1=new int[n];
+    int *s2=new int[n];
+    int *list=new int[n];
     for (i=0; i<n; i++) {
-        cin>>s[i];
+        cin>>s1[i];
     }
     for (i=0; i<n; i++) {
-        int j;
-        cin>>j;
-        for (int k=0; k<n; k++) {
-            list=push(list, j+s[k]);
-        }
+        cin>>s2[i];
     }
-    long sum;
+    s1=merge_sort(n, s1);
+    s2=merge_sort(n, s2);
     for (i=0; i<n; i++) {
-        sum+=top(list);
-        list=pop(list);
-        //print_list(length,list);
+        for (j=0; j<n/(i+1); j++) {
+            list=push(list, s1[i]+s2[j]);
+            if (length>n) {
+                list=pop(list);
+            }        }
+    }
+    for (i=0; i<n; i++) {
+        sum+=list[i];
     }
     cout<<sum;
     return 0;
