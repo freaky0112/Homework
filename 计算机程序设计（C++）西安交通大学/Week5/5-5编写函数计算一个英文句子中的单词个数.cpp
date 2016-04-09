613c47e242cedefba417c811cfb9991d225de266
freaky0112 <freakoutelf@gmail.com>
2016-04-09 15:43:01 +0800

    Week5 Solved

--- 计算机程序设计（C++）西安交通大学/Week5/5-5编写函数计算一个英文句子中的单词个数.cpp
+++ 计算机程序设计（C++）西安交通大学/Week5/5-5编写函数计算一个英文句子中的单词个数.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
+#include "string.h"
 using namespace std;
 
-int get_count(){
-	char a[100];
-	int i=0;
-	bool judge=true;
-	while(cin.get()!='\n'){
-		if (a[0]!='.'||a[0]!=','||a[0]!='?')
+int countplus(string origin,int count){
+	if (origin.length()>1)
+	{
+		int a=0,b=0,c=0;
+		for (int i = 0; i < origin.length(); ++i)
 		{
-			i++;
+			if (origin[i]=='.')
+			{
+				a++;
+			}else if (origin[i]==',')
+			{
+				b++;
+			}else if (origin[i]=='?')
+			{
+				c++;
+			}
 		}
-		cin>>a;
+		if (a+b+c!=origin.length())
+		{
+			count++;	
+		}
 
+	}else if (origin.length()==1)
+	{
+		/* code */
+		if (origin=="."||origin==","||origin=="?")
+		{
+			/* code */
+		}else{
+			count++;
+		}
+	}	
+	return count;
+}
+
+int get_count(){
+	string str;
+	getline(cin,str);
+	int count=0;
+	int start=0;
+	string origin;
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if (str[i]==' ')
+		{
+			origin=str.substr(start,i-start);
+			start=i+1;
+			count=countplus(origin,count);
+		}
 	}
-	return i;
+	origin=str.substr(start,str.length()-start);
+	count=countplus(origin,count);
+	return count;
 }
 
 int main(int argc, const char * argv[]) {
