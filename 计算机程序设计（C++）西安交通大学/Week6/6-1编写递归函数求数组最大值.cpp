01754045916fe569d79bb48d8411b536cae0502f
freaky0112 <freakoutelf@gmail.com>
2016-04-16 16:52:37 +0800

    sync to mac

---
+++ 计算机程序设计（C++）西安交通大学/Week6/6-1编写递归函数求数组最大值.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int max(int* list,int len){
+	if (len==1)
+	{
+		return list[0];
+	}else{
+		int a=list[0];
+		int b=max(list,len-1);
+		return a>b?a:b;
+	}
+}
+
+int main(int argc, const char * argv[]) {
+	int n;
+	int list[n];
+	cin>>n;
+	for (int i = 0; i < n; ++i){
+		cin>>list[i];
+	}
+	cout<<max(list,n);
+	return 0;
+}
